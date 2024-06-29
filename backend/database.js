6d18eb93cc7d4bea60e5f9d0fc2cc685074db1b7
Emmanuel Salcedo <mail@emmanuelsalcedo.com>
2024-06-28 21:13:13 -0700

    Add SSL configuration to Sequelize

--- backend/database.js
+++ backend/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // You might need to set this to true depending on your SSL certificate
+      rejectUnauthorized: false,
     },
   },
 });
