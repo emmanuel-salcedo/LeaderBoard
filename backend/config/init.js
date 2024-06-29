9e39b90aac4190c48df6fe260df5d3686f4e5108
Emmanuel Salcedo <mail@emmanuelsalcedo.com>
2024-06-28 22:14:08 -0700

    Fix paths and initialize database

--- backend/config/init.js
+++ backend/config/init.js
@@ -1,16 +1,20 @@
-const sequelize = require('../database');
+// config/init.js
+const sequelize = require('./database');
 const League = require('../backend/models/league');
 const Church = require('../backend/models/church');
 const Point = require('../backend/models/point');
 
-const initDb = async () => {
-  try {
+async function initializeDatabase() {
     await sequelize.sync({ force: true });
-    console.log("Database synced successfully!");
-  } catch (error) {
-    console.error("Error syncing database: ", error);
-  }
-};
 
-initDb();
+    // Example data
+    const league1 = await League.create({ name: 'League 1' });
+    const church1 = await Church.create({ name: 'Church 1', LeagueId: league1.id });
+    await Point.create({ description: 'Good Deed', points: 10, ChurchId: church1.id });
 
+    console.log('Database initialized!');
+}
+
+initializeDatabase().catch(error => {
+    console.error('Error initializing database:', error);
+});
