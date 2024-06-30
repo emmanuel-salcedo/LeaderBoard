97e4d499320905274e93881ab13ba8ffacdb1ad0
Emmanuel Salcedo <mail@emmanuelsalcedo.com>
2024-06-29 18:39:05 -0700

    Ensure correct database configuration

--- config/database.js
+++ config/database.js
@@ -3,13 +3,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  logging: false, // Disable logging in production
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // This is important for Heroku Postgres
-    },
-  },
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
